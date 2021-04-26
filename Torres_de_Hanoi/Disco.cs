a0cdad2a89e67ff04410f55d008c0bac2d378750
Pau <blanespau@gmail.com>
2021-04-26 13:19:11 +0200

    implementacion clase disco

--- Torres_de_Hanoi/Disco.cs
+++ Torres_de_Hanoi/Disco.cs
@@ -8,11 +8,12 @@ namespace Torres_de_Hanoi
 {
     class Disco
     {
-        /*TODO: 
-        Decidir tipo de Valor
         public int Valor { get; set; }
-        public String Valor { get; set; }
-        */
+
+        public Disco(int v)
+        {
+            Valor = v;
+        }
 
     }
 }
