5bbe84d97ac904c17dfb65ea49cf3e4e91f52b8d
Pau <blanespau@gmail.com>
2021-04-26 13:28:00 +0200

    clase hanoi

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -8,14 +8,100 @@ namespace Torres_de_Hanoi
 {
     class Hanoi
     {
+
+        int m = 0;
+        int direccion = 0;
         /*TODO: Implementar métodos*/
         public void mover_disco(Pila a, Pila b)
         {
-
+            if ((a.Top > b.Top || a.Top == 0) && b.Top != 0)
+            {
+                a.push(b.pop());
+                direccion = 1;
+                m++;
+            }
+            else if ((b.Top > a.Top || b.Top == 0) && a.Top != 0)
+            {
+                b.push(a.pop());
+                direccion = 2;
+                m++;
+            }
         }
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
         {
+            if (n % 2 == 1 || n == 1)
+            {
+                while (fin.Size < n)
+                {
+                    mover_disco(ini, fin);
+                    if (direccion == 2)
+                    {
+                        Console.WriteLine("Moviendo de inicio a fin");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Moviendo de fin a inicio");
+                    }
+                    if (fin.Size == n) return m;
+                    mover_disco(ini, aux);
+                    if (direccion == 2)
+                    {
+                        Console.WriteLine("Moviendo de inicio a aux");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Moviendo de aux a inicio");
+                    }
+                    mover_disco(aux, fin);
+                    if (direccion == 2)
+                    {
+                        Console.WriteLine("Moviendo de aux a fin");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Moviendo de fin a aux");
+                    }
+                    if (fin.Size == n) return m;
+
+                }
+            }
+            else
+            {
+                while (fin.Size < n)
+                {
+                    mover_disco(ini, aux);
+                    if (direccion == 2)
+                    {
+                        Console.WriteLine("Moviendo de inicio a aux");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Moviendo de aux a inicio");
+                    }
+                    mover_disco(ini, fin);
+                    if (direccion == 2)
+                    {
+                        Console.WriteLine("Moviendo de inicio a fin");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Moviendo de fin a inicio");
+                    }
+                    if (fin.Size == n) return m;
+                    mover_disco(aux, fin);
+                    if (direccion == 2)
+                    {
+                        Console.WriteLine("Moviendo de aux a fin");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Moviendo de fin a aux");
+                    }
+                    if (fin.Size == n) return m;
+
+                }
+            }
             return 0;
         }
 
